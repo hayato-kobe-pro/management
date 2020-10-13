e039b703dcb0d492f078aea46a8860d14b0a4284
masao <masao>
2020-10-13 13:44:01 +0900

    簡単な修正完了

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,16 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+rails g scaffold schedules 
+
 module Management
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    # application.rb
+    config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,3 +23,6 @@ module Management
     # the framework and any gems in your application.
   end
 end
+
+
+ 
